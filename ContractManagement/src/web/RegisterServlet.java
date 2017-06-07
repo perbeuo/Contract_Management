d35b206ca7045bef30fb1b92aa2500ae8c6f2f15
perbeuo <perbeuo@outlook.com>
2017-06-07 18:11:56 +0800

    Interface Overhaul

--- ContractManagement/src/web/RegisterServlet.java
+++ ContractManagement/src/web/RegisterServlet.java
@@ -25,60 +25,41 @@ public class RegisterServlet extends HttpServlet {
 		//Get the user's name, password and repeat password
 		String name = request.getParameter("name");
 		String password = request.getParameter("password");
-		String password2 = request.getParameter("password2");
 		
 		//Declare the identifies of handling registration
 		boolean flag = false;
 		//Initialize the prompt message 
 		String message = "";
-		//Instantiate the object of entity class User 
-		User user = new User();
-		// Initialize the user business logic class
-		UserService userService = new UserService();
-				
-		/*
-		 * Handle registration request, verify the following:
-		 * 1.user name, password, and the repeated password can not be empty.
-		 * 2.Repeated password and password should keep consistent.
-		 * 
-		 * If fails to meet the above requirements, the registration failed.
-		 */
-		
-		if(name == "" || password == "" || password2 == ""){
-			System.out.println("Entered incorrectly! ---");
-			System.out.println("User name, password, and the repeated password can not be empty!");
-			message = "User name, password, and the repeat password can not be empty!";	
-		} else if(!password2.equals(password)){
-			System.out.println("Entered incorrectly!---");
-			System.out.println("Repeated password and password should keep consistent!");
-			message = "Repeat password and password should keep consistent!";
-		} else {
-			try {
-				// Encapsulate the user information to object of user entity class object User 
-				user.setName(name);
-				user.setPassword(password);
-				// Call business logic for user registration
-				flag = userService.register(user);
-				if (flag) {
-					// If Registration is successful, set the prompt message
-					message = "Registration Successful!";
-					System.out.println("Registration Successful!");
-				} else {
-					// If Registration is failed, set the prompt message
-					message = "Registration failed!";
-					System.out.println("Registration failed!");
-				}
-			} catch (AppException e) {
-				e.printStackTrace();
-				// If the system exception, set the prompt message
-				message = "System exception!";
+		try {
+			//Instantiate the object of entity class User  
+			User user = new User();
+			// Initialize the user business logic class
+			UserService userService = new UserService();
+			// Encapsulate the user information to the user object
+			user.setName(name);
+			user.setPassword(password);
+			// Call business logic layer for user registration 
+			flag = userService.register(user);
+			if (flag) {
+				// After registration Successful, redirect to the login page
+				message = "Registration Succeed";
+				request.setAttribute("message", message);
+				//response.sendRedirect("toLogin");
+				request.getRequestDispatcher("/toLogin").forward(request,
+						response);
+			} else {
+				// Set prompt message
+				message = "Registration failed";
+				request.setAttribute("message", message); // Save prompt message into request 
+				// Forward to the registration page
+				request.getRequestDispatcher("/register.jsp").forward(request,
+						response);
 			}
+		} catch (AppException e) {
+			e.printStackTrace();
+			// Redirect to the exception page
+			response.sendRedirect("toError");
 		}
-		//Save message into request
-		request.setAttribute("message", message);
-		// Forward to the registration page
-		request.getRequestDispatcher("/toRegister").forward(request,
-				response);
 	}
 
 	/**
